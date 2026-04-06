d03905b5d742684e0536aa6bd714a62b85d34a51
Tuan <Tuan>
2026-04-06 13:38:20 +0700

    Them huong dan

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # CyberTruck
+# co 3 cach chay 
+./hihi  #chay cho MPC
+./hihi pp #chay cho PP
+./hihi mpc #chay cho MPC luon
+./hihi abc #vi du nhu nay se bao loi
--- src/main.cpp
+++ src/main.cpp
@@ -5,24 +5,23 @@
 int main(int argc, char* argv[]) {
     std::cout << "=== Lane Keeping System ===\n";
 
-    std::string videoPath = "/dev/video0";
-    ControlMode mode = ControlMode::PURE_PURSUIT; // default
-
+    std::string videoPath = "/dev/video0";   // cố định
+    ControlMode mode = ControlMode::MPC; // default
     if (argc > 1) {
-        videoPath = argv[1];
-    }
-    if (argc > 2) {
-        std::string mode_str = argv[2];
-
+        std::string mode_str = argv[1];
         if (mode_str == "mpc")
             mode = ControlMode::MPC;
         else if (mode_str == "pp")
             mode = ControlMode::PURE_PURSUIT;
+        else {
+            std::cerr << "[ERROR] Invalid mode! Use: mpc or pp\n";
+            return 1;
+        }
     }
 
     try {
         Logic logic(videoPath);
-        logic.setControlMode(mode);   
+        logic.setControlMode(mode);
         logic.run();
     } catch (const std::exception& e) {
         std::cerr << "[FATAL] " << e.what() << "\n";
